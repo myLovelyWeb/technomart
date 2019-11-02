13c59a2defcb4c883281d9026f71b1e92a4262b1
myLovelyWeb <mylovelyweb@mail.ru>
2019-11-02 06:37:51 +0300

    added disabled style for elements

--- js/modal-cart-message.js
+++
@@ -1,7 +0,0 @@
-var linkModaCartMessage = document.querySelectorAll(".card__btn--buy");
-var modaCartMessage = document.querySelector(".modal-cart-message");
-
-linkModaCartMessage.addEventListener("click", function (evt) {
-  evt.preventDefault();
-  modaCartMessage.classList.add("modal--show");
-}); 
